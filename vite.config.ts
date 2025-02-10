6d9ffd73c32675bdf9cdaff2d91a29c1b701efdb
quentinkrammer <bopatzable@gmail.com>
2025-02-10 19:57:48 +0100

    local plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// const jsToBottomNoModule = () => {
+//   return {
+//     name: "no-attribute",
+//     //@ts-ignore
+//     transformIndexHtml(html) {
+//       html = html.replace(`type="module" crossorigin`, "");
+//       //@ts-ignore
+//       let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0];
+//       html = html.replace(scriptTag, "");
+//       html = html.replace("<!-- # INSERT SCRIPT HERE -->", scriptTag);
+//       return html;
+//     },
+//   };
+// };
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
